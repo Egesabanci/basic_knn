700e1f4aa3565d43de572e6d69cfa3d59be8be53
Ege Sabancı <egesabanci@outlook.com.tr>
2020-10-17 01:19:29 +0300

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="basic_knn",
-    version="0.0.1",
+    version="0.0.2",
     author="Ege Sabancı",
     author_email="egesabanci@outlook.com.tr",
     description="K-Nearest Neighbors algorithm for classification problems.",
